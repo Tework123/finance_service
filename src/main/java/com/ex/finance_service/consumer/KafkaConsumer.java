8bd145351c11a805df9056ce25ce9b7f311fbdbb
Tework123 <daniliesaec@gmail.com>
2025-08-18 22:13:00 +0300

    kafka2

--- src/main/java/com/ex/finance_service/consumer/KafkaConsumer.java
+++ src/main/java/com/ex/finance_service/consumer/KafkaConsumer.java
@@ -1,10 +1,9 @@
 package com.ex.finance_service.consumer;
 
 import com.ex.finance_service.dto.FinanceServiceDto.SendRouteEventsRequestDto;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -25,5 +24,17 @@ public class KafkaConsumer {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @KafkaListener(topics = "request-topic", groupId = "my-json-group")
+    @SendTo  // <- Spring использует reply-topic из заголовка
+    public String listenSync(String message){
+        try {
+            SendRouteEventsRequestDto.RouteEventDto dto = objectMapper.readValue(message, SendRouteEventsRequestDto.RouteEventDto.class);
+            System.out.println("Kafka consumer received message: " + dto.getRouteEventId());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "yes, i am accept you message";
     }
 }
